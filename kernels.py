f29f585ea0085cb515afd6d75a01f1705b985187
josejoll-wq <josejoll@ucm.es>
2026-03-05 18:21:10 +0100

    Add functions for sumar, escalar, and difuminar

--- kernels.py
+++ kernels.py
@@ -6,6 +6,41 @@ def incr(core_id: int, gpu_mem: GPUMemory, sm_mem: SMMemory, _: Barrier) -> None
         idx = sm_mem.ini_bloque + core_id
         gpu_mem.res[idx] = gpu_mem.dato1[idx] + 1
 
+def sumar(core_id, gpu_mem: GPUMemory, sm_mem: SMMemory, _: Barrier)-> None:
+    if core_id < sm_mem.tam_bloque:
+        idx = sm_mem.ini_bloque + core_id
+        gpu_mem.res[idx] = gpu_mem.dato1[idx] + gpu_mem.dato2[idx]
+
+
+def escalar(core_id: int, gpu_mem: GPUMemory, sm_mem: SMMemory, _: Barrier) -> None:
+    if core_id < sm_mem.tam_bloque:
+        idx = sm_mem.ini_bloque + core_id
+        gpu_mem.res[idx] = gpu_mem.dato1[idx] * gpu_mem.dato2[idx]
+
+
+def difuminar(core_id: int, gpu_mem: GPUMemory, sm_mem: SMMemory, barrera : Barrier) -> None:
+    if core_id < sm_mem.tam_bloque:
+        idx_global = sm_mem.ini_bloque + core_id
+        sm_mem.datos[core_id] = gpu_mem.dato1[idx_global]
+
+        barrera.wait()
+
+        suma = 0.0
+        contador = 0
+        for rango in range(-2, 3):
+            idx_relativo = core_id + rango
+
+            if 0 <= idx_relativo < sm_mem.tam_bloque:
+                suma += sm_mem.datos[idx_relativo]
+                contador += 1
+            else:
+                idx_vecino_global = idx_global + rango
+                if 0 <= idx_vecino_global < gpu_mem.tam_datos.value:
+                    suma += gpu_mem.dato1[idx_vecino_global]
+                    contador += 1
+
+        gpu_mem.res[idx_global] = suma / contador
+
 INCR = 1
 SUMAR = 2
 DIFUMINAR = 3
